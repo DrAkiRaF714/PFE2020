b459e2f9d50fea5547759b8e76a9f9644bb35261
ACEBES valentin <acebesvalentin.va@gmail.com>
2020-08-31 23:34:48 +0200

    [ADD] Component to main page and first render debug

--- Mplayz/imports/ui/App.jsx
+++ Mplayz/imports/ui/App.jsx
@@ -1,11 +1,11 @@
 import React from 'react';
-import { Hello } from './Hello.jsx';
-import { Info } from './Info.jsx';
 
-export const App = () => (
-  <div>
-    <h1>Welcome to Meteor!</h1>
-    <Hello/>
-    <Info/>
-  </div>
-);
+const App = () => {
+  return(
+    <div>
+      <h1>Welcome to Mplayz!</h1>
+    </div>
+  );
+};
+
+export default App;
